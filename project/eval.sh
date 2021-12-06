d6bfe6f1799322991680bbefacd5a2d7861e81ac
Jared Fernandez <jaredfern@cmu.edu>
2021-12-06 18:02:24 -0500

    Add task loop to train and eval scripts

--- project/eval.sh
+++ project/eval.sh
@@ -1,19 +1,25 @@
-export GLUE_DIR=./glue_data
-export TASK_NAME=QQP
+export GLUE_DIR=/data/datasets/GLUE
+export TASKS="CoLA MNLI MRPC QNLI QQP RTE SST-2 STS-B WNLI"
 
-CUDA_VISIBLE_DEVICES=0 python ./run_glue_with_pabee.py \
-  --model_type distilbert \
-  --model_name_or_path ./output/ \
-  --task_name $TASK_NAME \
-  --do_eval \
-  --do_lower_case \
-  --data_dir "$GLUE_DIR/$TASK_NAME" \
-  --max_seq_length 128 \
-  --per_gpu_eval_batch_size 1 \
-  --learning_rate 2e-5 \
-  --logging_steps 50 \
-  --num_train_epochs 15 \
-  --output_dir ./output/ \
-  --eval_all_checkpoints \
-  --patience 3,6 \
-  --benchmark
+export MODEL_NAME=distilbert
+export OUTPUT_DIR=/data/output
+
+for TASK in $TASKS; do
+  CUDA_VISIBLE_DEVICES=2 python ./run_glue_with_pabee.py \
+    --model_type $MODEL_NAME \
+    --model_name_or_path /$OUTPUT_DIR/$TASK_NAME/checkpoint-500 \
+    --task_name $TASK_NAME \
+    --do_eval \
+    --do_lower_case \
+    --data_dir "$GLUE_DIR/$TASK_NAME" \
+    --max_seq_length 128 \
+    --per_gpu_eval_batch_size 1 \
+    --learning_rate 2e-5 \
+    --logging_steps 50 \
+    --num_train_epochs 15 \
+    --output_dir ./output/ \
+    --eval_all_checkpoints \
+    --tokenizer $MODEL_NAME-base-uncased \
+    --patience 1,2,3,6 \
+    --benchmark
+  done
--- project/train.sh
+++ project/train.sh
@@ -1,22 +1,26 @@
-export GLUE_DIR=../PABEE/glue_data/
-export TASK_NAME=QQP
+export GLUE_DIR=/data/datasets/GLUE
+export OUTPUT_DIR=/data/output
+export TASKS="CoLA MNLI MRPC QNLI QQP RTE SST-2 STS-B WNLI"
 
-CUDA_VISIBLE_DEVICES=0 python ./run_glue_with_pabee.py \
-  --model_type distilbert \
-  --model_name_or_path distilbert-base-uncased \
-  --task_name $TASK_NAME \
-  --do_train \
-  --do_eval \
-  --do_lower_case \
-  --data_dir "$GLUE_DIR/$TASK_NAME" \
-  --max_seq_length 128 \
-  --per_gpu_train_batch_size 64 \
-  --per_gpu_eval_batch_size 64 \
-  --learning_rate 2e-5 \
-  --save_steps 2000 \
-  --logging_steps 2000 \
-  --num_train_epochs 1 \
-  --output_dir ./output/ \
-  --lazy_model_loading \
-  --evaluate_during_training
+for TASK in $TASKS; do
+    CUDA_VISIBLE_DEVICES=2 python ./run_glue_with_pabee.py \
+      --model_type $MODEL_NAME \
+      --model_name_or_path $MODEL_NAME-base-uncased \
+      --task_name $TASK \
+      --do_train \
+      --do_eval \
+      --do_lower_case \
+      --data_dir "$GLUE_DIR/$TASK" \
+      --max_seq_length 128 \
+      --per_gpu_train_batch_size 64 \
+      --per_gpu_eval_batch_size 1 \
+      --learning_rate 2e-5 \
+      --save_steps 500 \
+      --logging_steps 1000 \
+      --num_train_epochs 1 \
+      --output_dir $OUTPUT_DIR/$TASK  \
+      --tokenizer $MODEL_NAME-base-uncased \
+      --patience 2,3,4,6,8 \
+      --evaluate_during_training;
+  done
 

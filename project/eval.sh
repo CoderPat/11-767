084309b3e9f23029506ae8e19b8ecc0456d4263c
Jared Fernandez <jaredfern@cmu.edu>
2021-12-09 14:00:38 -0500

    local bash scripts

--- project/eval.sh
+++ project/eval.sh
@@ -1,13 +1,14 @@
 export GLUE_DIR=/data/datasets/GLUE
+# export TASKS="CoLA MNLI MRPC QQP RTE SST-2 STS-B WNLI"
 export TASKS="QNLI"
 
 export MODEL_NAME=bert
-export OUTPUT_DIR=/data/jaredfer/odml-bert-pabee-reversed
+export OUTPUT_DIR=/data/jaredfer/odml/odml-bert-pabee
 
 for TASK in $TASKS; do
   CUDA_VISIBLE_DEVICES=2 python3 ./run_glue_with_pabee.py \
     --model_type $MODEL_NAME-pabee \
-    --model_name_or_path /$OUTPUT_DIR/$TASK \
+    --model_name_or_path $OUTPUT_DIR/$TASK \
     --task_name $TASK \
     --do_eval \
     --do_lower_case \
@@ -17,10 +18,9 @@ for TASK in $TASKS; do
     --learning_rate 2e-5 \
     --logging_steps 50 \
     --num_train_epochs 15 \
-    --output_dir ./output/ \
+    --output_dir $OUTPUT_DIR/$TASK \
     --eval_all_checkpoints \
     --tokenizer $MODEL_NAME-base-uncased \
-    --patience 1,2,3,6 \
-    --lazy_model_loading \
-    --benchmark
+    --patience 12
   done
+
--- project/train.sh
+++ project/train.sh
@@ -1,11 +1,11 @@
 export GLUE_DIR=/data/datasets/GLUE
-export TASKS="QNLI CoLA MNLI MRPC QQP RTE SST-2 STS-B WNLI"
+export TASKS="CoLA MNLI MRPC QQP RTE SST-2 STS-B WNLI"
 
 export MODEL_NAME=bert-pabee
-export OUTPUT_DIR=/data/jaredfer/odml-bert-pabee-reversed
+export OUTPUT_DIR=/data/jaredfer/odml-bert-pabee-reversed-1
 
 for TASK in $TASKS; do
-    CUDA_VISIBLE_DEVICES=3 python ./run_glue_with_pabee.py \
+    CUDA_VISIBLE_DEVICES=2 python ./run_glue_with_pabee.py \
       --model_type $MODEL_NAME \
       --model_name_or_path bert-base-uncased \
       --task_name $TASK \
@@ -17,7 +17,7 @@ for TASK in $TASKS; do
       --per_gpu_train_batch_size 64 \
       --per_gpu_eval_batch_size 1 \
       --learning_rate 2e-5 \
-      --save_steps 500 \
+      --save_steps 1000 \
       --logging_steps 1000 \
       --num_train_epochs 3 \
       --output_dir $OUTPUT_DIR/$TASK  \
